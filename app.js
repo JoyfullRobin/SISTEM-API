383977c629d1f531206b3b97409283af9fc53f8f
JoyfullRobin <thiagonara1@gmail.com>
2024-08-06 21:12:04 -0300

    subindo projeto-api

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require ('express');
+const app = express();
+const sequelize = require('./config/config');
+
+const User = require('./models/user');
+
+sequelize.sync()
+    .then(
+        () => {
+            console.log('Database conected and synced');            
+        })
+        .catch(
+            (error) => {
+                console.error('Unable to connect to the database: ',error);
+            }
+        )
+
+module.exports = app;
