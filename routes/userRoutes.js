c96c715026d5c98e49f0523481eb8aa9a44445e3
JoyfullRobin <thiagonara1@gmail.com>
2024-08-15 22:02:30 -0300

    ajustes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-router.get ('/users', userController.listUsers);
+router.get('/users', userController.listUsers);
 
 module.exports = router
 
