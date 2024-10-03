ce8c2deeb19a78d3896ce5fffd541332d515d795
JoyfullRobin <thiagonara1@gmail.com>
2024-10-03 19:42:49 -0300

    test

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,3 +15,37 @@ exports.listUsers = async (req, res) => {
             res.status(500).send(error.mensagem);
     }
 };
+
+exports.activeUsers = async(req, res) => {
+    try {
+        const {id} = req.params;
+        const user = await User.findOne({where: {id: id}});
+        
+        if (user){
+            user.active = true;
+            await user.save();
+            res.status(200).json({message: 'User activated successfuly'});
+        } else{
+            res.status(400).send('User not found')
+        }
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+exports.deactiveUsers = async(req, res) => {
+    try {
+        const {id} = req.params;
+        const user = await User.findOne({where: {id: id}});
+        
+        if (user){
+            user.active = false;
+            await user.save();
+            res.status(200).json({message: 'User deactivated successfuly'});
+        } else{
+            res.status(400).send('User not found')
+        }
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
